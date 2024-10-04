a7e1db63045370bfa53046f1107f839f01bb6d0c
Adil <59019368+Adil-Mukhtar@users.noreply.github.com>
2024-10-04 23:55:00 +0500

    CORS Policies implemented

--- src/main/java/com/adil/social/config/AppConfig.java
+++ src/main/java/com/adil/social/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.adil.social.config;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -24,14 +31,43 @@ public class AppConfig {
                 .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
-                //.httpBasic().and() //needed for some api secuirty working with password
-                .csrf(csrf -> csrf.disable());
+                //.httpBasic().and() //needed for some api security working with password
+                .csrf(csrf -> csrf.disable())
+                //CORS Policy Configuration
+                .cors(cors->cors.configurationSource(corsConfigurationSource()));
+
+
 
         return http.build();
+    }
+
+
+    private CorsConfigurationSource corsConfigurationSource() {
+
+
+        return new CorsConfigurationSource() {
+
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:3000/"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList(
+                        "Authorizatoin"));
+                cfg.setMaxAge(3600L);
+
+                return cfg;
+            }
+        };
     }
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
 }
